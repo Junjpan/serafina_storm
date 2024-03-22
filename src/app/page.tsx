749f3067447eb662db998d1fb4fd376434897362
JunjPan <elisepjj@hotmail.com>
2024-03-21 21:00:11 -0700

    toggle theme in html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import { themeContext } from "./context/themeContext";
 import axios from "axios";
 import "./page.scss";
+import { Theme } from "./context/type";
 
 export default function Home() {
   const { theme, setTheme } = useContext(themeContext);
@@ -13,11 +14,25 @@ export default function Home() {
     });
   }, []);
 
-  console.log(theme);
+  /**
+   * Add theme to html,
+   */
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.classList.remove("dark-mode");
+    } else {
+      document.documentElement.classList.add("dark-mode");
+    }
+  }, [theme]);
+
+  const changeTheme = (): void => {
+    let currentTheme: Theme = theme === "light" ? "dark" : "light";
+    setTheme && setTheme(currentTheme);
+  };
 
   return (
     <main className="main">
-      <button onClick={() => setTheme && setTheme("dark")}>Change Theme</button>
+      <button onClick={() => changeTheme()}>Change Theme</button>
     </main>
   );
 }
